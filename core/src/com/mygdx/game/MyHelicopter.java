35a0227e9755883b651bf1f0cd4bd92d6003b9b9
Monika Luu <monika-luu@hotmail.com>
2023-01-29 14:06:29 +0100

    show coordinates on screen

--- core/src/com/mygdx/game/MyHelicopter.java
+++ core/src/com/mygdx/game/MyHelicopter.java
@@ -2,6 +2,8 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -12,6 +14,8 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class MyHelicopter extends State implements ApplicationListener {
 	SpriteBatch batch;
 
+	BitmapFont font;
+
 	public static final int WIDTH = 480;
 	public static final int HEIGHT = 800;
 	private HelicopterSprite helicopterSprite;
@@ -21,22 +25,23 @@ public class MyHelicopter extends State implements ApplicationListener {
 	public void create ()
 	{batch = new SpriteBatch();
 	helicopterSprite = new HelicopterSprite(100, 400);
+	font = new BitmapFont();
+	font.setColor(Color.BLACK);
 	}
 	@Override
 	public void resize(int width, int height) {
 
 	}
-
+// Task 2a
 	@Override
 	public void handleInput() {
 		if (Gdx.input.justTouched()) {
 			if (Gdx.input.getX() > (helicopterSprite.getHelicopter().getWidth()/2) && Gdx.input.getX() < WIDTH - (helicopterSprite.getHelicopter().getWidth()/2)){
 				if (Gdx.input.getY() >= helicopterSprite.getHelicopter().getHeight( )|| Gdx.input.getY() < HEIGHT - helicopterSprite.getHelicopter().getHeight()) {
-					helicopterSprite.getPosition().y =HEIGHT - (Gdx.input.getY() +( helicopterSprite.getHelicopter().getHeight() / 2));
+					helicopterSprite.getPosition().y = HEIGHT - (Gdx.input.getY() +( helicopterSprite.getHelicopter().getHeight() / 2));
 					helicopterSprite.getPosition().x = (Gdx.input.getX() - (helicopterSprite.getHelicopter().getWidth()/2));
 				}
 			}
-
 		}
 	}
 
@@ -45,7 +50,12 @@ public class MyHelicopter extends State implements ApplicationListener {
 		ScreenUtils.clear(1, 1, 1, 1);
 		batch.begin();
 		batch.draw(helicopterSprite.getNewHelicopter(), helicopterSprite.getPosition().x, helicopterSprite.getPosition().y);
+		font.draw(batch, "Sprite Position: " + helicopterSprite.getPosition().x + ", " + helicopterSprite.getPosition().y ,
+				30, 770);
+
+
 		batch.end();
+
 		handleInput();
 	}
 
@@ -63,5 +73,6 @@ public class MyHelicopter extends State implements ApplicationListener {
 	@Override
 	public void dispose () {
 		batch.dispose();
+		font.dispose();
 	}
 }
